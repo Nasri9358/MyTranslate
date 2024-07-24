38bf072ebadcedda996406bee8dbc368445d6194
ankiton <ankiton@yandex.ru>
2024-07-24 18:22:35 +0300

    Создание оболочки Translate v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,9 @@ android {
 
 dependencies {
 
-    implementation (org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version)
-    implementation (com.squareup.retrofit2:retrofit:2.9.0)
-    implementation (com.squareup.retrofit2:converter-gson:2.9.0)
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
